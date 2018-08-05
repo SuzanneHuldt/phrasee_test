b8068768a8369b4b4e67b05fafa468d785632e04
Suzanne Huldt <suzannegchuldt@gmail.com>
2018-08-05 16:38:32 +0100

    likes and comments refactored to own methods

--- Grouper.js
+++ Grouper.js
@@ -1,29 +1,36 @@
 "use strict";
 
+const likes = [];
+const comments = [];
 
 class Grouper{
-
   groupNotifications(id, notifications){
-    let likes = [];
-    let replies = [];
     var i;
     var notification;
     for(i = 0; i < notifications.length; i++){
       notification = notifications[i];
-      if(notification.post.id == id){
-        if(notification.type == "Like"){
-          likes.push(notification.user.name)
-        }
-      }
-      if(notification.post.id == id){
-        if(notification.type == "Comment"){
-          replies.push(notification.user.name)
-          replies.push(notification.comment.commentText)
-        }
-      }
+      this.getLikes(notification, id);
+      this.getComments(notification, id);
     }
     console.log(likes)
-    console.log(replies)
+    console.log(comments)
+  }
+
+  getLikes(notification, id){
+    if(notification.post.id == id){
+      if(notification.type == "Like"){
+        likes.push(notification.user.name)
+      }
+    }
+  }
+
+  getComments(notification, id){
+    if(notification.post.id == id){
+      if(notification.type == "Comment"){
+        comments.push(notification.user.name)
+        comments.push(notification.comment.commentText)
+      }
+    }
   }
 }
 
